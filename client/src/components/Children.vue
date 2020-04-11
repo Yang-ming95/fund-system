546d0c4d83b80e663159147b95e5b05763cf7f04
Yang-ming95 <978707940@qq.com>
2020-04-11 23:52:05 +0800

    初始化后台管理系统

---
+++ client/src/components/Children.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="hello">
+      <div class="children" @click="say">
+        我是子组件
+        <div>
+          父组件对我说：{{msg}}
+        </div>
+      </div>
+  
+    </div>
+  </template>
+  
+  <script>
+  
+    export default {
+        //父组件通过props属性传递进来的数据
+        props: {
+            msg: {
+                type: String,
+                default: () => {
+                    return ''
+                }
+            }
+        },
+        data () {
+          return {
+              childrenSay: 'hello, parent'
+          }
+        },
+  
+        methods: {
+            // 子组件通过emit方法触发父组件中定义好的函数，从而将子组件中的数据传递给父组件
+            say(){
+                this.$emit('say' , this.childrenSay);
+            }
+        }
+    }
+  </script>
---
+++ client/src/views/Parent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="parent">
+      我是父组件
+      <!--父组件监听子组件触发的say方法，调用自己的parentSay方法-->
+      <!--通过:msg将父组件的数据传递给子组件-->
+      <children :msg="msg" @say="parentSay"></children>
+      <button @click="changemsg">aaa</button>
+    </div>
+  </template>
+  
+  <script>
+  
+  import Children from '@/components/Children.vue'
+  
+  export default {
+    data () {
+      return {
+        msg: 'hello, children'
+      }
+    },
+  
+    methods: {
+        // 参数就是子组件传递出来的数据
+        parentSay(msg){
+            console.log(msg) // hello, parent
+        },
+        changemsg(){
+          this.msg='change, children'
+        }
+    },
+  
+    // 引入子组件
+    components:{
+        children: Children
+    }
+  }
+  </script>
